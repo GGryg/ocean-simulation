162346a27b80ca7305318fcfd5eb5471d4912e46
GGryg <shiryon@wp.pl>
2024-04-07 19:56:33 +0200

    Add camera

--- include/camera.h
+++ include/camera.h
@@ -1,9 +1,34 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <glm/glm.hpp>
+
+
+// Implementation of camera using Euler's angles
 class Camera
 {
+public:
+    Camera(glm::vec3 position_t, glm::vec3 up_t, float yaw_t, float pitch_t);
 
+    glm::mat4 getViewMat() const;
+
+    void processMouse(float xoffset, float yoffset);
+
+    float zoom() const;
+private:
+    void updateVectors();
+
+    glm::vec3 m_position;
+    glm::vec3 m_front;
+    glm::vec3 m_up;
+    glm::vec3 m_right;
+    glm::vec3 m_worldUp;
+
+    float yaw;
+    float pitch;
+    float movementSpeed;
+    float mouseSensitivity;
+    float m_zoom;
 };
 
 #endif
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,1 +1,55 @@
 #include "camera.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+Camera::Camera(glm::vec3 position_t, glm::vec3 worldUp_t, float yaw_t, float pitch_t)
+    : m_position{position_t}
+    , m_front{glm::vec3{0.0f, 0.0f, -1.0f}} 
+    , m_worldUp{worldUp_t}
+    , yaw{yaw_t}
+    , pitch{pitch_t}
+    , movementSpeed{2.0f}
+    , mouseSensitivity{0.5f}
+{
+    updateVectors();
+}
+
+glm::mat4 Camera::getViewMat() const
+{
+    return glm::lookAt(m_position, m_position + m_front, m_up);
+}
+
+void Camera::processMouse(float xoffset, float yoffset)
+{
+    xoffset *= mouseSensitivity;
+    yoffset *= mouseSensitivity;
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if(pitch > 89.0f)
+    {
+        pitch = 89.0f;
+    }
+    if(pitch < -89.0f)
+    {
+        pitch = -89.0f;
+    }
+
+    updateVectors();
+}
+
+float Camera::zoom() const
+{
+    return m_zoom;
+}
+
+void Camera::updateVectors()
+{
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    m_front = glm::normalize(front);
+    m_right = glm::normalize(glm::cross(m_front, m_worldUp));
+    m_up = glm::normalize(glm::cross(m_right, m_front));
+}
