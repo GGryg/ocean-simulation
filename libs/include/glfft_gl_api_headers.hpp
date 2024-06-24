e2d4da0c0d5535e7d1d27b954f3ae5e361f3edb0
GGryg <shiryon@wp.pl>
2024-06-25 01:10:09 +0200

    Refactor

--- libs/include/glfft_gl_api_headers.hpp
+++ libs/include/glfft_gl_api_headers.hpp
@@ -1,11 +1,11 @@
 //
-// This for GLFFT to use GLAD
+// This for GLFFT to use glad
 //
 
 #ifndef GLFFT_GL_API_HEADERS_HPP
 #define GLFFT_GL_API_HEADERS_HPP
 
-#include <GLAD/gl.h>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 #define GLFFT_GLSL_LANG_STRING "#version 430 core\n"
